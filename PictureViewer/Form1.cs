5442040fc32a1259f3301a9518fd8f2f954dc582
pmatla95 <pmatla95@gmail.com>
2016-06-14 11:01:02 -0400

    added README

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -16,5 +16,48 @@ namespace PictureViewer
         {
             InitializeComponent();
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //show the open file dialog. If the user clicks OK, load the picture that the user chose
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+        //these functions are event handlers for the UI in the design page
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear the photo
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            //show the color dialog box. If the user clicks OK, change the pictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            /* if the user selects the stretch check box,
+             * change the pictureboxs sizemode to strech, if the user clears, change it back to norm */
+             if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+             else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+Peter Matla, Apprenda Summer 2016 Visual Studio App Testing
+
+6/14/16
+
+Visual Studio C# App for uploading photos with background color.
